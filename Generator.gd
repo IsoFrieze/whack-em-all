7575fe7f6913a734286c31da84414dd0e5a071b2
Alex Losego <shinx@isofrieze.com>
2024-03-15 10:08:20 -0400

    added point counter
    point counter

--- Generator.gd
+++ Generator.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
 @onready var root = $"."
-var rng = RandomNumberGenerator.new()
+@onready var points = $Points
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 # reference to the current object on screen
-var current_item
+var current_item: Node
+
+# number of objects killed
+var kill_count: int = 0
 
 # distance from edge of screen to spawn objects
-var BUFFER = 100
+var BUFFER: int = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,12 +29,17 @@ func _on_clickme_killed():
 	root.remove_child(current_item)
 	# add a new one
 	generate_item()
+	
+	# update kill count
+	kill_count += 1
+	points.text = str("Points: ", kill_count)
+	
 	pass
 
 # create a new object to click
 func generate_item():
 	# load the scene
-	var scene = load("res://ClickMe.tscn")
+	var scene: PackedScene = load("res://ClickMe.tscn") 
 	current_item = scene.instantiate()
 	# add it to the tree
 	root.add_child(current_item)
