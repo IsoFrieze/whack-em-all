656fa46430acb235f0e9a7df4bb4778d36c2f2e5
Jemmysponz <jemmysponz7@gmail.com>
2024-03-15 00:09:58 -0700

    added a dumb little spritesheet I quickly scribbled out and set the cursor to be it TODO: figure out how to spit it into frames and animate it on click

---
+++ Cursor.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_custom_mouse_cursor(preload("res://whack mockup.png"))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
